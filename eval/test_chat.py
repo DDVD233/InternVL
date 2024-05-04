7d70ad4e39a88adf9da893fc1692a44a48d8ae3d
DVD <zjdavid.2003@gmail.com>
2024-05-04 10:07:44 -0700

    Move internvl to root package

--- eval/test_chat.py
+++ eval/test_chat.py
@@ -6,11 +6,13 @@ from transformers import AutoModel, CLIPImageProcessor, CLIPProcessor
 from transformers import AutoTokenizer
 from datasets.IemocapDataset import IemocapDataset
 from torchvision.utils import save_image
+from internvl.model.internvl_chat import InternVLChatModel
 
 # Initialize IemocapDataset
-iemocap_dataset = IemocapDataset('/home/dvd/data/depression_interview/dataset/IEMOCAP_full_release/IEMOCAP_full_release')
+iemocap_dataset = IemocapDataset(
+    '/home/dvd/data/depression_interview/dataset/IEMOCAP_full_release/IEMOCAP_full_release')
 path = "OpenGVLab/InternVL-Chat-V1-1"
-model = AutoModel.from_pretrained(
+model = InternVLChatModel.from_pretrained(
     path,
     torch_dtype=torch.bfloat16,
     low_cpu_mem_usage=True,
@@ -25,30 +27,31 @@ if not os.path.exists(output_path):
     os.makedirs(output_path)
 
 for index, sample in enumerate(iemocap_dataset):
-    frames: torch.Tensor = sample['frames']
-    for index_f, frame in enumerate(frames):
-        pixel_values = image_processor(images=frame, return_tensors='pt').pixel_values
-        pixel_values = pixel_values.to(torch.bfloat16).cuda()
+    frames: torch.Tensor = sample['frames']  # 4, H, W, C
+    # make 2x2 grid of frames, shape = 2H, 2W, C
+    grid = torch.cat([torch.cat([frames[0], frames[1]], dim=1),
+                      torch.cat([frames[2], frames[3]], dim=1)], dim=0)
 
-        generation_config = dict(
-            num_beams=1,
-            max_new_tokens=512,
-            do_sample=False,
-        )
+    # for index_f, frame in enumerate(frames):
+    pixel_values = image_processor(images=grid, return_tensors='pt').pixel_values
+    pixel_values = pixel_values.to(torch.bfloat16).cuda()
 
-        question = "Please describe the image."
-        response = model.chat(tokenizer, pixel_values, question, generation_config)
-        print('-' * 50)
-        print(response)
-        print('-' * 50)
-        # save image to output_path
-        image = save_image(frame, os.path.join(output_path, f'{index}_{index_f}.jpg'))
-        # save response to output_path
-        with open(os.path.join(output_path, f'{index}_{index_f}.txt'), 'w') as file:
-            file.write(response)
+    generation_config = dict(
+        num_beams=1,
+        max_new_tokens=512,
+        do_sample=False,
+    )
 
-
-
+    question = "Please describe the image."
+    response = model.chat(tokenizer, pixel_values, question, generation_config)
+    print('-' * 50)
+    print(response)
+    print('-' * 50)
+    # save image to output_path
+    save_image(grid, os.path.join(output_path, f'{index}.jpg'))
+    # save response to output_path
+    with open(os.path.join(output_path, f'{index}.txt'), 'w') as file:
+        file.write(response)
 
 # path = "OpenGVLab/InternVL-Chat-V1-1"
 # model = AutoModel.from_pretrained(
