69b8f4ed3e75e86b8a5b5464ce1f786eae0760bf
DVD <zjdavid.2003@gmail.com>
2024-06-06 21:34:08 -0700

    Add dataset definition

--- internvl_chat/internvl/train/internvl_chat_audio_finetune.py
+++ internvl_chat/internvl/train/internvl_chat_audio_finetune.py
@@ -299,9 +299,8 @@ class LazySupervisedDataset(Dataset):
         else:
             images = [image]
 
-        audio_path = data_item['audio_path']
-        # Audio is located at self.root/../audio_path
-        audio_path = os.path.join(os.path.dirname(self.root), audio_path)
+        audio_path = data_item['audio']
+        audio_path = os.path.join(self.root, audio_path)
         audio_info = process_audio(audio_path)
 
         audios = audio_info["input_audios"]
